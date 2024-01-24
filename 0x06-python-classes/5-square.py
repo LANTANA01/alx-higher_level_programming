5b66b7a7733399ca2f5d7b981049e0ecd7a60d53
LANTANA01 <ojimahtijani@gmail.com>
2024-01-23 18:02:13 -0800

    python OOP

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,36 @@
-#!/usr/bin/python3
+#!/usr/bin/python5
 """Defines the class module"""
 
 class Square:
-"""Represents the class Square"""
-    def __init__(self, size):
-        """Initializes a new square.
+    """Represents the class Square"""
+    def __init__(self, size=0):
+         """Initializes a new square.
 
         Args:
             size (int): The size of the new square.
         """
-
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """Set the current size of the square."""
-        return (self.__size)
+        """Sets the current size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         """Returns the current area of the square."""
-        return (self.__size * self.__size)
+        return self.__size ** 2
 
     def my_print(self):
-        """Prints the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
