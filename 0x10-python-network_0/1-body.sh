d0d9987e85c2acfb607f53e91d30ae177800869c
Lantana Tijani <144739521+LANTANA01@users.noreply.github.com>
2024-04-26 01:37:31 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o - -w '%{http_code}' -X GET "$1" 2>/dev/null | awk '/^2[0-9][0-9]/ {print}'
+curl -s -w "\n" -o /dev/null -D - "$1"
