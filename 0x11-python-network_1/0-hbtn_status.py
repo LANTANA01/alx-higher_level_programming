838035391c26e3cb8d1fb56b3c9afc9cdf86947b
Lantana Tijani <144739521+LANTANA01@users.noreply.github.com>
2024-04-27 00:35:28 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+if __name__ == '__main__':
+    import urllib.request
 
   with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
