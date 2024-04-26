aaee141d4dc5dc92ea0d4c2e3b578cab5e770d3f
LANTANA01 <ojimahtijani@gmail.com>
2024-04-25 18:16:48 -0700

    clientURL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a URL, sends a GET request to the URL, and displays the body of the response
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") && [ "$status_code" -eq 200 ] && echo "Route 2" && curl -s "$1"
+curl -s -w "%{http_code}\n" -o /dev/null -D - "$1" | grep -q 200 && echo "Route 2"
