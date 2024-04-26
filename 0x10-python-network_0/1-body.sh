b78d99e3dec61af69c6b8bd8bd3ccb22ceb897e0
Lantana Tijani <144739521+LANTANA01@users.noreply.github.com>
2024-04-26 01:54:37 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -w "%{http_code}\n" -o /dev/null -D - "$1" | grep -q 200 && echo "Route 2"
