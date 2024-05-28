3eb4eaacc93816f473d19274e6e547571e42d530
LANTANA01 <ojimahtijani@gmail.com>
2024-05-28 09:19:07 -0700

    Js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Print a number if argv is a num, else print NaN
 
-if(process.argv[2] == null || isNaN(process.argv[2])){
-	console.log('Not a number');
-}else{
-	console.log('My number:',parseInt(process.argv[2]));
+if (process.argv[2] == null || isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number:',parseInt(process.argv[2]));
 }
